f749d754187fec010dfbd35f74388b3dfc31e9e2
Steve Elliott <steve@tegud.net>
2018-05-16 23:33:13 +0100

    write yml to serverless.yml

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const fs = require("fs");
+const yaml = require("js-yaml");
+
 const { loadConfig } = require("./lib/config-loader");
 const { expandToServerlessConfig } = require("./lib/serverless");
 
-const writeServerlessConfig = async () => {};
+const writeServerlessConfig = async (config, outputFile) => new Promise((resolve, reject) => fs.writeFile(`${process.cwd()}/${outputFile}`, yaml.safeDump(config, { skipInvalid: true }), "utf-8", (err) => {
+    if (err) {
+        return reject(err);
+    }
+
+    return resolve();
+}));
 
 const program = require("commander");
 
@@ -10,15 +19,12 @@ program
     .description("Configures checkless to the configured AWS environment");
 
 program
-    .command("generate [file]")
+    .command("generate [file] [outputFile]")
     .description("Generates serverless config for the checkless config")
     .action(async (file = "checkless.yml", outputFile = "serverless.yml") => {
         console.log(`Loading config from ${file}`);
 
         const config = await loadConfig(`${process.cwd()}/${file}`);
-
-        console.log(JSON.stringify(config, null, 4));
-
         const serverlessConfig = expandToServerlessConfig(config);
 
         await writeServerlessConfig(serverlessConfig, outputFile);
--- tests/serverless.test.js
+++ tests/serverless.test.js
@@ -10,40 +10,77 @@ describe("expand to serverless config", () => {
                 },
             },
         })).toEqual({
-            service: "checkless",
             custom: {
-                requestCompleteTopic: "site-check-complete",
                 checkCompleteTopic: "check-complete",
                 checkFailedTopic: "check-failed",
-            },
-            provider: {
-                name: "aws",
-                runtime: "nodejs8.10",
-                iamRoleStatements: [
-                    {
-                        Effect: "Allow",
-                        Resource: "*",
-                        Action: ["sns:*"],
-                    },
-                ],
+                requestCompleteTopic: "site-check-complete",
             },
             functions: {
                 "make-request": {
-                    handler: "make-request",
                     events: [
                         {
                             schedule: {
                                 rate: "rate(5 minutes)",
                                 input: {
-                                    url: "http://localhost/",
                                     region: "eu-west-1",
                                     snsTopic: "${self:custom.requestCompleteTopic}", // eslint-disable-line no-template-curly-in-string
+                                    url: "http://localhost/",
                                 },
                             },
                         },
                     ],
+                    handler: "make-request",
+                },
+                "handle-request": {
+                    handler: "handle-request.handleRequest",
+                    environment: {
+                        completeSnsTopic: "${self:custom.checkCompleteTopic}", // eslint-disable-line no-template-curly-in-string
+                        failedSnsTopic: "${self:custom.checkFailedTopic}", // eslint-disable-line no-template-curly-in-string
+                        region: "eu-west-1",
+                    },
+                    events: [
+                        { sns: "${self:custom.requestCompleteTopic}" }, // eslint-disable-line no-template-curly-in-string
+                    ],
+                },
+                "send-to-slack": {
+                    handler: "send-to-slack.sendToSlack",
+                    environment: {
+                        webhookUrl: undefined,
+                    },
+                    events: [
+                        { sns: "${self:custom.checkCompleteTopic}" }, // eslint-disable-line no-template-curly-in-string
+                    ],
                 },
             },
+            provider: {
+                iamRoleStatements: [
+                    {
+                        Action: ["sns:*"],
+                        Effect: "Allow",
+                        Resource: "*",
+                    },
+                ],
+                name: "aws",
+                runtime: "nodejs8.10",
+            },
+            resources: {
+                Resources: {
+                    MailQueue: {
+                        Properties: {
+                            DisplayName: "Checkless",
+                            Subscription: [
+                                {
+                                    Endpoint: undefined,
+                                    Protocol: "email",
+                                },
+                            ],
+                            TopicName: "${self:custom.checkFailedTopic}", // eslint-disable-line no-template-curly-in-string
+                        },
+                        Type: "AWS::SNS::Topic",
+                    },
+                },
+            },
+            service: "checkless",
         });
     });
 });
