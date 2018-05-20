a2a6a297ed6ae7f970d9212e1bf4472545df8d4a
Steve Elliott <steve@tegud.net>
2018-05-20 22:54:17 +0100

    config by region

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const { expandToServerlessConfig } = require("./lib/serverless");
 
 const signale = require("signale");
 const program = require("commander");
+const { stat, mkdir } = require("fs");
+const { copy } = require("fs-extra");
+const { promisify } = require("util");
+
+const promiseStat = promisify(stat);
+const mkDirStat = promisify(mkdir);
+
+const createFolderIfNeeded = async (directory) => {
+    try {
+        return await promiseStat(directory);
+    } catch (error) {
+        // Do nothing
+    }
+
+    signale.info(`Creating directory: ${directory}`);
+    return mkDirStat(directory);
+};
+
+const copyNodeModulesToRegionDirectory = async (directory) => {
+    signale.info(`Copying node_modules to: ${directory}`);
+
+    return copy(`${process.cwd()}/node_modules`, `${directory}/node_modules`);
+};
+
+const createServerlessDeploy = async (region, outputFile, config) => {
+    const rootDirectory = `${process.cwd()}/.checkless`;
+    const directory = `${rootDirectory}/${region}`;
+
+    await createFolderIfNeeded(rootDirectory);
+
+    await createFolderIfNeeded(directory);
+
+    await copyNodeModulesToRegionDirectory(directory);
+
+    await writeConfig(config, `${directory}/${outputFile}`);
+};
 
 module.exports = () => {
     program
@@ -16,11 +52,27 @@ module.exports = () => {
             signale.info(`Loading config from ${file}`);
 
             const config = await loadConfig(`${process.cwd()}/${file}`);
+
+            signale.info(`Loaded config from ${file}`);
+
             const serverlessConfig = expandToServerlessConfig(config);
 
-            await writeConfig(serverlessConfig, `${process.cwd()}/${outputFile}`);
+            const regionConfigs = Object.keys(serverlessConfig);
 
-            signale.success(`Serverless config written to: ${outputFile}`);
+            signale.info(`Regions to install to: ${regionConfigs.join(", ")}`);
+
+            try {
+                await Promise.all(regionConfigs.map(region => createServerlessDeploy(
+                    region,
+                    outputFile,
+                    serverlessConfig[region],
+                )));
+            } catch (error) {
+                signale.error("Error writing serverless config", error);
+                return;
+            }
+
+            signale.success("Serverless configs written");
         });
 
     program.parse(process.argv);
