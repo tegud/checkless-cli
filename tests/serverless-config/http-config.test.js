bbf9d077a70faea75a9accc50000b72155ef587e
Steve Elliott <steve@tegud.net>
2018-07-21 09:56:16 +0100

    need to be separate function

--- tests/serverless-config/http-config.test.js
+++ tests/serverless-config/http-config.test.js
@@ -1,25 +1,21 @@
 const { expandToServerlessConfig } = require("../../lib/serverless-config");
 
 describe("httpEnabled", () => {
-    it("sets aws lambda proxy event to do check", async () => {
+    it("sets aws lambda proxy event to trigger check", async () => {
         expect(expandToServerlessConfig({
             region: "eu-west-1",
-            checks: {
-                localhost: {
-                    url: "http://localhost/",
-                    regions: ["eu-west-1"],
-                    checkEvery: "never",
-                },
-            },
             httpEnabled: true,
-        })["eu-west-1"].functions["make-request"].events).toEqual([
-            {
-                http: {
-                    path: "check",
-                    method: "post",
-                    cors: true,
+        })["eu-west-1"].functions["trigger-check"]).toEqual({
+            handler: "node_modules/checkless/trigger-check.triggerCheck",
+            events: [
+                {
+                    http: {
+                        path: "check",
+                        method: "post",
+                        cors: true,
+                    },
                 },
-            },
-        ]);
+            ],
+        });
     });
 });
